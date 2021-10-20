21173466c6c309d58ec60a536cf283ad55a2a10a
flo_h <flo.1244@gmail.com>
2021-10-19 21:16:44 -0400

    Cleaned up code

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+	
+const inputs = document.querySelectorAll(".controls input");//selects all inputs.
+
+const handleUpdate = function() {
+	// console.log(this.value);
+	  const suffix = this.dataset.sizing || ''; //dataset is a object that contains all the data attributes for a specific element.
+	  //console.log(suffix);
+	  //console.log(name):
+	  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix); //allows the value to be updated.
+};
+  
+/*ARROW EXPRESSION ES6*/	
+//inputs.forEach(input => input.addEventListener("change", handleUpdate));
+//inputs.forEach(input => input.addEventListener("mouseover", handleUpdate));	
+
+	
+// Loops over each inputs
+inputs.forEach(function(input){
+	input.addEventListener("change", handleUpdate);
+	input.addEventListener("mousemove", handleUpdate);
+});
+	
